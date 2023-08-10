da3faf979eccbb5a7eac8e6f2260e971dc6d365c
Alina Frenchel <alina.frenchel@gmail.com>
2023-08-10 13:27:55 +0300

    setting up proj-routes

---
+++ src/features/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Alina</p>
+    </header>
+  );
+};
+
+export default Header;
